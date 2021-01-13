4edcc477d6b3fe1f913dec3beafe84aa9bade392
Clément Piat <clement.piat2@gmail.com>
2021-01-13 15:09:24 +0100

    hyperoptimization: first commit

---
+++ hyperoptimization.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=gr_ho
+#SBATCH --output=%x.o%j
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu
+#SBATCH --mem=64gb
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+
+# To clean and load modules defined at the compile and link phases
+module purge
+module load anaconda3/2020.02/gcc-9.2.0
+
+# Activate anaconda environment
+source activate /gpfs/users/piatc/.conda/envs/gr
+
+# Go to the directory where the job has been submitted 
+cd ${SLURM_SUBMIT_DIR}
+
+# Execution
+python -u hyperoptimization.py -d french_tweets.csv -n 100 --max_size 1e5
