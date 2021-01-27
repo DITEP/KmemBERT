b792a477fc8f2b7c424919799d8eb2f46cce1a2f
Clément Piat <clement.piat2@gmail.com>
2021-01-27 14:20:20 +0100

    concatenate: back to livre

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -27,7 +27,7 @@ with open(file_path_concatenated, "w") as outfile:
                 for i, line in enumerate(infile):
                     row = line.split(sep)
                     if len(row) == 9:
-                        outfile.write('|'.join(row))
+                        outfile.write('£'.join(row))
                     else:
                         n_errors+=1
                         
