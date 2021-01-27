ab2494e0d5bf8fde3b15c64df049c771a92cbe7f
Clément Piat <clement.piat2@gmail.com>
2021-01-27 11:39:12 +0100

    concatenate: fix

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 import os
 import pandas as pd
 
@@ -16,14 +15,17 @@ with open(file_path_concatenated, "w") as outfile:
         if "dcd.txt" in file_name or  "texteSimbad" in file_name or "p2012.txt" in file_name:
             print(file_name)        
             with open(os.path.join(path_data, file_name)) as infile:
-                print(header)
                 header = infile.readline()
                 if len(header.split('£')) == 9:
-                    df = pd.read_csv(os.path.join(path_data, file_name), sep='£', encoding='utf-8', engine='python', error_bad_lines=False)
+                    sep = '£'
                 elif len(header.split('|')) == 9:
-                    df = pd.read_csv(os.path.join(path_data, file_name), sep='|', error_bad_lines=False)
+                    sep = '|'
                 else:
                     continue
 
-                df.to_csv(file_path_concatenated, header=header, index=None, sep=str('£'), mode='a', encoding='utf-8') 
-                header = None
+                for i, line in enumerate(infile, 1):
+                    row = line.split(sep)
+                    if len(row) == 9:
+                        outfile.write('£'.join(row))
+                    else:
+                        print(f"Error with line {i}")
