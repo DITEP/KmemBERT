f978028a90d0ce3e73ad388ad057fe6199ded893
Clément Piat <clement.piat2@gmail.com>
2021-01-27 10:57:13 +0100

    concatenate_files: adapt to different file types

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -2,24 +2,26 @@
 import os
 import pandas as pd
 
-#test
 path_data= "/data/isilon/centraleNLP"
 file_name_concatenated = "concatenate.txt"
-list_files = os.listdir(path_data)
-
 file_name_wanted = ["dcd.txt", "texteSimbad*"]
-header_is_written = False
-with open(os.path.join(path_data, file_name_concatenated), 'w') as outfile:
+
+header = ["Noigr","clef","Date deces","Date cr","Code nature","Nature doct","Sce","Contexte","Texte"]
+
+list_files = os.listdir(path_data)
+file_path_concatenated = os.path.join(path_data, file_name_concatenated)
+with open(file_path_concatenated, "w") as outfile:
     for file_name in list_files:
         if "dcd.txt" in file_name or  "texteSimbad" in file_name or "p2012.txt" in file_name:
-            print(file_name)
-            df = pd.read_csv(os.path.join(path_data, file_name), sep="£", nrows=10)
-            print(df.shape[1])
-            print(df.columns)
+            print(file_name)        
             with open(os.path.join(path_data, file_name)) as infile:
-                if header_is_written:
-                    _ = infile.readline()
+                header = infile.readline()
+                if len(header.split("£")) == 9:
+                    df = pd.read_csv(os.path.join(path_data, file_name), sep="£", engine="python")
+                elif len(header.split("|")) == 9:
+                    df = pd.read_csv(os.path.join(path_data, file_name), sep="|")
                 else:
-                    header_is_written = True
-                    
-                outfile.write(infile.read())      
+                    continue
+
+                df.to_csv(file_path_concatenated, header=header, index=None, sep="£", mode="a") 
+                header = None
