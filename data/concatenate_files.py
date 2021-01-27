0ab8a9788b9e40a8dc9c1a6aa4d59e6d0b88188e
Clément Piat <clement.piat2@gmail.com>
2021-01-27 08:26:08 +0100

    concatenate_file: get rid of headers

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -6,12 +6,14 @@ file_name_concatenated = "concatenate.txt"
 list_files = os.listdir(path_data)
 
 file_name_wanted = ["dcd.txt", "texteSimbad*"]
-
+header_is_written = False
 with open(os.path.join(path_data, file_name_concatenated), 'w') as outfile:
     for file_name in list_files:
         if "dcd.txt" in file_name or  "texteSimbad" in file_name or "p2012.txt" in file_name:
-            with open(os.path.join(path_data, file_name)) as infile:
-                outfile.write(infile.read())
-
-
-
+            with open(os.path.join(path_data, file_name), "w+") as infile:
+                if header_is_written:
+                    _ = infile.readline()
+                else:
+                    header_is_written = True
+                    
+                outfile.write(infile.read())      
