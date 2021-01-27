aae94f89068c3a305a790f5da7706ab03f0287b6
Clément Piat <clement.piat2@gmail.com>
2021-01-27 09:18:11 +0100

    concatenate_files.py: see df columns

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 #test
 path_data= "/data/isilon/centraleNLP"
@@ -11,6 +12,9 @@ with open(os.path.join(path_data, file_name_concatenated), 'w') as outfile:
     for file_name in list_files:
         if "dcd.txt" in file_name or  "texteSimbad" in file_name or "p2012.txt" in file_name:
             print(file_name)
+            df = pd.read_csv(os.path.join(path_data, file_name), sep="£", nrows=10)
+            print(f"{df.shape[1]} columns")
+            print(df.columns)
             with open(os.path.join(path_data, file_name)) as infile:
                 if header_is_written:
                     _ = infile.readline()
