bf4ccae65c4faf71cd978bda554d2c0e1811fe46
clementpiat <clement.piat2@gmail.com>
2021-02-03 10:03:39 +0100

    concatenate_file: try a fix

--- data/concatenate_files.py
+++ data/concatenate_files.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
 import os
 import pandas as pd
 
 path_data= "/data/isilon/centraleNLP"
 file_name_concatenated = "concatenate.txt"
 file_name_wanted = ["dcd.txt", "texteSimbad*"]
+sep = "\xc2\xa3"
+new_sep = "gr_random_separator_impossible_that_it_appears"
+new_line = "\n"
 
 columns = ["Noigr","clef","Date deces","Date cr","Code nature","Nature doct","Sce","Contexte","Texte"]
 
@@ -16,22 +18,18 @@ with open(file_path_concatenated, "w") as outfile:
             print(file_name)        
             with open(os.path.join(path_data, file_name)) as infile:
                 header = infile.readline()
-                if len(header.split('£')) == 9:
-                    sep = '£'
-                elif len(header.split('|')) == 9:
-                    sep = '|'
-                else:
+                if len(header.split(sep)) != 9:
                     continue
 
                 if columns:
-                    outfile.write('£'.join(columns) + u'\r\n')
+                    outfile.write(sep.join(columns) + new_line)
                     columns = None
 
                 n_errors = 0
                 for i, line in enumerate(infile):
                     row = line.split(sep)
-                    if len(row) == 9 and (sep=='£' or len(line.split('£'))==1):
-                        outfile.write('£'.join(row) + u'\r\n')
+                    if len(row) == 9:
+                        outfile.write(sep.join(row) + new_line)
                     else:
                         n_errors+=1
                         
