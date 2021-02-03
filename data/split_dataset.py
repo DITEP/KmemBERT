0270c4ed7eabaca6ccf70cef5642544dcf215e12
clementpiat <clement.piat2@gmail.com>
2021-02-03 13:06:14 +0100

    split dataset: split on noigr

--- data/split_dataset.py
+++ data/split_dataset.py
@@ -4,14 +4,15 @@ This code splits the concatenated dataset into a train and test one.
 """
 import pandas as pd 
 import os
+from sklearn.model_selection import train_test_split
 
 train_size = 0.7
 seed = 0
 
 data_path = "/data/isilon/centraleNLP"
 file_path = os.path.join(data_path, "concatenate.txt")
-train_path = os.path.join(data_path, "train_dataset.csv")
-test_path = os.path.join(data_path, "test_dataset.csv")
+train_path = os.path.join(data_path, "train.csv")
+test_path = os.path.join(data_path, "test.csv")
 
 
 if os.path.isfile(train_path) or os.path.isfile(test_path):
@@ -20,7 +21,10 @@ if os.path.isfile(train_path) or os.path.isfile(test_path):
 
 df = pd.read_csv(file_path, sep='\xc2\xa3', engine='python')
 
-train=df.sample(frac=train_size, random_state=seed) 
+noigrs = pd.unique(df["label"])
+train_noigrs, test_noigrs = train_test_split(noigrs, train_size=train_size, random_state=seed)
+
+train=df[df["label"].isin(train_noigrs)]
 test=df.drop(train.index)
 
 train.to_csv(train_path, index=False)
