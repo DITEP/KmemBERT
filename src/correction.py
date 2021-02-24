2bbdd1ac78e250aa97ff7b0ebf62ea7ff80e8ef0
ayoumen <qabel1998@outlook.fr>
2021-02-24 10:40:10 +0100

    Added SymSpell Checker

--- src/correction.py
+++ src/correction.py
@@ -15,6 +15,10 @@ from pandarallel import pandarallel
 
 from utils import get_root
 
+import pkg_resources
+from symspellpy import SymSpell, Verbosity
+from time import time
+
 nlp = spacy.load('fr')
 pandarallel.initialize(progress_bar=True)
 
@@ -27,17 +31,25 @@ def main(dataset, distance):
 
     spell = SpellChecker(language='fr',distance=distance)
 
-    def transform_one_sentence(sentence):
+    def transform_one_sentence(sentence, method='SpellChecker'):
         """
         Transforms a sentence as a string into another setence with the unknown words being corrected
-        """
-        words = list(map(str, nlp(sentence)))
-        return ' '.join(list(map(spell.correction, words)))
+        # """
+
+        if method=='SymSpell':
+            sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+            sym_spell.load_dictionary('../data/fr-100k.txt', term_index=0, count_index=1)
+            suggestions = sym_spell.lookup_compound(sentence, max_edit_distance=2)
+            return suggestions[0].term
+            
+        elif method=='SpellChecker':
+            words = list(map(str, nlp(sentence)))
+            return ' '.join(list(map(spell.correction, words)))
 
     path_root = get_root()
     path_dataset = os.path.join(path_root, "data", args.dataset)
     df = pd.read_csv(path_dataset)
-    df.text = df.text.parallel_apply(transform_one_sentence)
+    df.text = df.text.parallel_apply(transform_one_sentence, method= args.method)
     
     correction_dataset = f'_correction_{distance}.csv'.join(path_dataset.split(".csv"))
     print(f"Saving the corrected dataset into {correction_dataset}...")
@@ -46,11 +58,12 @@ def main(dataset, distance):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset", type=str, default="french_tweets_short.csv", 
+    parser.add_argument("-d", "--dataset", type=str, default="../data/french_tweets_short.csv", 
         help="dataset filename")
     parser.add_argument("-n", "--distance", type=int, default=2, 
         help="distance parameter for SpellChecker")
-
+    parser.add_argument("-m", "--method", type=str, default='SpellChecker', 
+        help="Method of correction")
     args = parser.parse_args()
     print(f"\n> args:\n{json.dumps(vars(args), sort_keys=True, indent=4)}\n")
     main(args.dataset, args.distance)
