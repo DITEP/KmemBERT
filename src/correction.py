0eaf75df5a60bd9faf57f73ce5965656920bd4b1
ayoumen <qabel1998@outlook.fr>
2021-02-24 12:00:28 +0100

    Added SymSpell

--- src/correction.py
+++ src/correction.py
@@ -41,7 +41,7 @@ def main(dataset, distance):
             sym_spell.load_dictionary('../data/fr-100k.txt', term_index=0, count_index=1)
             suggestions = sym_spell.lookup_compound(sentence, max_edit_distance=2)
             return suggestions[0].term
-            
+
         elif method=='SpellChecker':
             words = list(map(str, nlp(sentence)))
             return ' '.join(list(map(spell.correction, words)))
@@ -49,11 +49,7 @@ def main(dataset, distance):
     path_root = get_root()
     path_dataset = os.path.join(path_root, "data", args.dataset)
     df = pd.read_csv(path_dataset)
-<<<<<<< HEAD
-    df.text = df.text.parallel_apply(transform_one_sentence, method= args.method)
-=======
-    df["Texte"] = df["Texte"].parallel_apply(transform_one_sentence)
->>>>>>> 130c271cf6641ad94d8f94254de81e49fced46e0
+    df["Texte"] = df["Texte"].parallel_apply(transform_one_sentence, method=args.method)
     
     correction_dataset = f'_correction_{distance}.csv'.join(path_dataset.split(".csv"))
     print(f"Saving the corrected dataset into {correction_dataset}...")
