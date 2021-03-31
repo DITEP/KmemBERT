b96402c48ab7cc64fa2202a393bfc08b227914b6
clementpiat <clement.piat2@gmail.com>
2021-03-31 10:18:11 +0200

    testing: put back confusion matrix

--- src/testing.py
+++ src/testing.py
@@ -110,7 +110,7 @@ def test(model, test_loader, config, path_result, epoch=-1, test_losses=None, va
     metrics['accuracy'] = accuracy_score(bin_labels, bin_predictions)
     metrics['balanced_accuracy'] = balanced_accuracy_score(bin_labels, bin_predictions)
     metrics['f1_score'] = f1_score(bin_labels, bin_predictions, average=None).tolist()
-    # metrics['confusion_matrix'] = confusion_matrix(bin_labels, bin_predictions).tolist()
+    metrics['confusion_matrix'] = confusion_matrix(bin_labels, bin_predictions).tolist()
     metrics['correlation'] = float(np.corrcoef(predictions, test_labels)[0,1])
 
     try:
