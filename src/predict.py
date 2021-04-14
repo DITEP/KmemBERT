e6cba81fadeaff8dd61362e3d6401da298cd3ba9
ayoumen <qabel1998@outlook.fr>
2021-04-14 12:51:28 +0200

    predict

--- src/predict.py
+++ src/predict.py
@@ -33,16 +33,17 @@ def main(args):
     # config.resume = "training_21-04-05_10h02m00s"
 
     model = HealthBERT("cpu", config)
-    #file_to_classify = pd.read_csv(config.data_path, nrows = config.nrows)
-    #text_to_classify = file_to_classify.Texte
+    file_to_classify = pd.read_csv(config.data_path, nrows = config.nrows)
+    text_to_classify = file_to_classify.Texte
 
-    text_to_classify = ["il va bientôt mourir", "Le patient va très bien, son corps se comporte bien, il va bientôt guérir","Aujourd'hui, il y a eu une très grande amélioration de l'état du patient","Le patient va mourir en moins d'une semaine, c'est alertant"]
+    #text_to_classify = ["il va bientôt mourir", "Le patient va très bien, son corps se comporte bien, il va bientôt guérir","Aujourd'hui, il y a eu une très grande amélioration de l'état du patient","Le patient va mourir en moins d'une semaine, c'est alertant"]
     class_names = ["Moins de trois mois", "Plus de trois mois"]
 
     explainer = LimeTextExplainer(class_names=class_names)
 
-    exp = explainer.explain_instance(text_to_classify[0], predict, num_features=4)
+    exp = explainer.explain_instance(text_to_classify[0], predict, num_features=15,num_samples=100)
     print(exp.as_list())
+
 
 
 if __name__ == '__main__':
@@ -52,7 +53,7 @@ if __name__ == '__main__':
     parser.add_argument("-r", "--resume", type=str, required=True, 
         help="result folder in with the saved checkpoint will be reused")
     
-    parser.add_argument("-nr", "--nrows", type=int, default=None, 
+    parser.add_argument("-nr", "--nrows", type=int, default=1, 
         help="maximum number of samples for testing")
 
     main(parser.parse_args())
